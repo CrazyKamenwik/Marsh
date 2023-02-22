ea157c3931ea75c767aea74fcae72cb25a127d9d
Vlad <infin95@gmail.com>
2023-02-22 12:49:03 +0300

    - add message controller tests

---
+++ TicketSystem/TicketSystem.Tests/IntergationTests/ControllersTests/MessageControllerTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using TicketSystem.API;
+using TicketSystem.API.ViewModels.Messages;
+using TicketSystem.BLL.Models;
+using TicketSystem.DAL;
+using TicketSystem.Tests.IntegrationTests.Models;
+
+namespace TicketSystem.Tests.IntegrationTests.ControllerTests;
+
+public class MessageControllerIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _httpClient;
+    private const int UserId = 1;
+    private const int OperatorId = 2;
+    private const int OpenTicketId = 1;
+
+    public MessageControllerIntegrationTests(WebApplicationFactory<Program> factory)
+    {
+
+        factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                var inMemoryRoot = new InMemoryDatabaseRoot();
+                services.AddSingleton(inMemoryRoot);
+                services.AddDbContext<ApplicationContext>(optionsBuilder =>
+                    optionsBuilder.UseInMemoryDatabase("MyDb", inMemoryRoot));
+
+                using (var scoped = services.BuildServiceProvider().CreateScope())
+                {
+                    var db = scoped.ServiceProvider.GetRequiredService<ApplicationContext>();
+                    db.Database.EnsureDeleted();
+                    db.Database.EnsureCreated();
+                    InitializeData.Initialize(db);
+                }
+            });
+        });
+
+        _httpClient = factory.CreateClient();
+    }
+
+    [Theory]
+    [InlineData("Hi", UserId, null)]
+    [InlineData("Hello", UserId, null)]
+    [InlineData("Hello", OperatorId, OpenTicketId)]
+    [InlineData("Hi", OperatorId, OpenTicketId)]
+    public async Task Post_ValidMessage_ReturnsMessageViewModel(string text, int userId, int? ticketId)
+    {
+        // Arrange
+        var message = new ShortMessageViewModel { Text = text, UserId = userId, TicketId = ticketId };
+        var content = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _httpClient.PostAsync("/api/message", content);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var messageViewModel = JsonConvert.DeserializeObject<MessageViewModel>(responseContent);
+        Assert.Equal(message.Text, messageViewModel!.Text);
+    }
+
+    [Theory]
+    [InlineData("", UserId, null)]
+    [InlineData(null, UserId, null)]
+    [InlineData("Hello", 0, null)]
+    [InlineData("Hello", int.MinValue, null)]
+    public async Task Post_InvalidMessage_ReturnsBadRequest(string text, int userId, int? ticketId)
+    {
+        // Arrange
+        var message = new ShortMessageViewModel { Text = text, UserId = userId, TicketId = ticketId };
+        var content = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _httpClient.PostAsync("/api/message", content);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+}
