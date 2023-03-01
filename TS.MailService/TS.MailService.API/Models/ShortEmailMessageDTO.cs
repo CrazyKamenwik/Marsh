53823e9c618411c5c86ef02cdb0d2ee118162732
Vlad <infin95@gmail.com>
2023-03-01 10:25:23 +0300

    - add new DTO

--- TS.MailService/TS.MailService.API/Models/ShortEmailMessageDTO.cs
+++ TS.MailService/TS.MailService.API/Models/ShortEmailMessageDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace TS.MailService.Application.Models
+﻿using TS.MailService.Application.Enums;
+
+namespace TS.MailService.Application.Models
 {
-    public class ShortEmailMessageDTO
+    public class ShortEmailMessageDto
     {
+        public string Sender { get; set; } = null!;
+        public IEnumerable<string> Recipients { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string? Body { get; set; }
+        public IEnumerable<object>? Attachments { get; set; }
     }
 }
