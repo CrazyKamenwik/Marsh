8353caededbbf1c1cf80eebc05c1f0f139c50996
Vlad <infin95@gmail.com>
2023-03-09 16:09:18 +0300

    - Create API Gatewe

--- TS.ApiGateway/TS.ApiGateway/Program.cs
+++ TS.ApiGateway/TS.ApiGateway/Program.cs
@@ -1,30 +1,43 @@
+using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+using TS.ApiGateway;
 
 var builder = WebApplication.CreateBuilder(args);
+var routes = "Routes";
+
+builder.Configuration.AddOcelotWithSwaggerSupport(option => option.Folder = routes);
+
+builder.Services.AddOcelot(builder.Configuration).AddPolly();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+builder.Configuration.AddOcelot(routes, builder.Environment)
+    .AddEnvironmentVariables();
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+var aasds = builder.Configuration["ClientId"];
 
-await app.UseOcelot();
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+}).UseOcelot().Wait();
+
+app.MapControllers();
 
 app.Run();
